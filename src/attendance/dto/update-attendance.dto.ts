67101f43d7ced8f6c38a0967f524d02bc8febd83
Tarun Vadde <tarun@soulverse.us>
2025-10-31 22:45:35 +0530

    feat: update attendance dto
    Signed-off-by: Tarun Vadde <tarun@soulverse.us>

---
+++ src/attendance/dto/update-attendance.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsOptional, Min } from 'class-validator';
+
+export class UpdateAttendanceDto {
+  @ApiProperty({ description: 'Present count (days)', minimum: 0, required: false, example: 18 })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  presentCount?: number;
+}
